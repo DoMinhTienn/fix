128784588e42d102a13d2b1eaaa95868a519735f
giatruong2511 <giatruong251101@gmail.com>
2022-08-06 18:00:38 +0700

    fix

---
+++ BTL/src/main/java/com/dmt/configs/WebApplicationContextConfig.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dmt.configs;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Tien
+ */
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+    "com.dmt.controller"
+})
+public class WebApplicationContextConfig  implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer conf) {
+        conf.enable();
+    }
+    
+}
---
+++ BTL/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%-- 
+    Document   : index
+    Created on : Jul 25, 2022, 10:04:08 PM
+    Author     : Tien
+--%>
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<h1 class="text-center text-danger">DANH MUC SAN PHAM</h1>
+
